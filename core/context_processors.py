31df20d95bee9a1222a169bc2464847a45746cd2
rifa-sanjida <23101062@uap-bd.edu>
2025-10-16 22:11:59 +0600

    Update context_processors.py

--- core/context_processors.py
+++ core/context_processors.py
@@ -11,3 +11,19 @@ def cart_count(request):
     else:
         count = 0  # Guests don't have carts
     return {'cart_count': count}  # This number will be available in all templates
+def categories(request):
+    """This makes all categories available for the navigation menu"""
+    categories = Category.objects.all()  # Get every category from database
+    return {'categories': categories}  # Categories will be in every page's navigation
+
+def unread_messages_count(request):
+    """This shows how many unread messages a user has"""
+    if request.user.is_authenticated:  # Only for logged-in users
+        # Count messages where user is participant, message is unread, and not sent by user
+        count = Message.objects.filter(
+            conversation__participants=request.user,  # User is in conversation
+            is_read=False  # Message hasn't been read yet
+        ).exclude(sender=request.user).count()  # Don't count user's own messages
+    else:
+        count = 0  # Guests don't have messages
+    return {'unread_messages_count': count}  # This count shows in the navigation
